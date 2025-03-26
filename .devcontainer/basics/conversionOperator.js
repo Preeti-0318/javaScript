927286bd04925c90a0c63d679662fc0e71c79cac
Preeti-0318 <rajalaxmipaikaray60@gmail.com>
2025-03-26 18:37:02 +0000

    conversion of datatype

---
+++ .devcontainer/basics/conversionOperator.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+/*console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+let isLoggedIn = "preeti"
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(BooleanIsLoggedIn);
+// 1 = true; 0= false
+// " " =false
+//"preeti"= true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
