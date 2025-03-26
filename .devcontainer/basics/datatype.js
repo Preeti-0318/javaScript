1689970fc7f914faf4ea6c152f61e5ee49084ac9
Preeti-0318 <rajalaxmipaikaray60@gmail.com>
2025-03-26 18:02:05 +0000

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+//alert(3 + 3)// we are nodejs, not browser
+
+console.log(3 + 3);
+console.log("Preeti");
+console.log(typeof "preeti");
+console.log(typeof 5);
+console.log(typeof undefined);
+console.log(typeof  null);
+
+
+
