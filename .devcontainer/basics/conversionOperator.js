61ef4609c9d3128a7761c4fc682becf28aff55b0
Preeti-0318 <rajalaxmipaikaray60@gmail.com>
2025-03-26 18:53:11 +0000

    conversipn

--- .devcontainer/basics/conversionOperator.js
+++ .devcontainer/basics/conversionOperator.js
@@ -14,7 +14,25 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 // " " =false
 //"preeti"= true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(typeof stringNumber);
+
+// ************* Operations**************
+
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+//console.log(3 + 3);
+let str1 ="hello"
+let str2 ="preeti"
+let str3 = str1+str2
+console.log(str3);
+
+
+
+
+
+
 
