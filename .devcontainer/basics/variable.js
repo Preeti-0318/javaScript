740e57aa8723c8d3190f9608b47b6bb3db69e863
Preeti-0318 <rajalaxmipaikaray60@gmail.com>
2025-04-10 10:32:25 +0000

    variable

---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "rajalaxmi@gmail.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountState;
+
+//accountId = 2//not allowed
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword="122456"
+accountCity ="bangalore"
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
